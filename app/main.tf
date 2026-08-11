43a736a85f0f8fbbe29e1c72cb766409bdb7d386
mohitmalviya-skilltect <mohit.malviya@skilltect.com>
2026-08-11 12:44:38 +0530

    terraform test 1

--- app/main.tf
+++ app/main.tf
@@ -1,28 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
+resource "aws_security_group" "test" {
+  name = "test-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-test-public-bucket"
-
-  tags = {
-    Name = "Test Bucket"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "test_bucket" {
-  bucket = aws_s3_bucket.test_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
