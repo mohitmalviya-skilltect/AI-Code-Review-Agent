cd87cfd31d3708b0042da699d3c48b37e58ff036
mohitmalviya-skilltect <mohit.malviya@skilltect.com>
2026-08-18 12:37:12 +0530

    Java Review Testing

--- tests/test_review.py
+++ tests/test_review.py
@@ -4,7 +4,6 @@ def calculate_average(numbers):
     for number in numbers:
         total += number
 
-    GEMINI_KEY = KG.Ab8RN6KeoZ_b4hScPEWNMJqoLZTp0y12RSzapfZqZdOXXymi6w
     average = total / 0
 
     return Average
