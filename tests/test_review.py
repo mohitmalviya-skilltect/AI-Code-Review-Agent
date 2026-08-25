a904472a42d9e6d21d62ac06fe7196bf96681c95
mohitmalviya-skilltect <mohit.malviya@skilltect.com>
2026-08-25 17:20:36 +0530

    testing PR Review

--- tests/test_review.py
+++ tests/test_review.py
@@ -1,17 +1,18 @@
-def calculate_discount(price, discount):
-    total = price - discount
-    tax = total * 0.18
+def calculate_weekdays(start_date, end_date):
+    current_date = start_date
+    weekdays = 0
 
-    print("Price:", price)
-    print("Discount:", discount)
+    while current_date <= end_date:
+        if current_date.weekday() < 5:
+            weekdays += 1
 
-    final_price = total + tax
+        current_date += timedelta(days=1)
 
-    if final_price < 0:
-        return 0
-
-    return final_price
+    return weekdays
 
 
-def get_user_name(user):
-    return user["name"].upper()
+start = datetime(2026, 5, 8)
+end = datetime(2026, 8, 31)
+
+result = calculate_weekdays(start, end)
+print(f"Working days: {result}")
